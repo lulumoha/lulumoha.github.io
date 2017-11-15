b724ee70e7f6557fdb54d2a0705d0ff6e1e92d4f
lulumoha <lulumoha123@gmail.com>
2017-11-15 11:47:15 +0300

    add changes

--- js/calc.js
+++ js/calc.js
@@ -16,12 +16,10 @@ var divide = function(number1, number2){
 
 // Everything below this line is user interface (or front-end) logic:
 $(document).ready(function() {
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+  $("form#add").submit(function(event) {
+ event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
 alert(add(number1, number2));
 });
-
-var number1 = parseInt($("#add1").val());
-var number2 = parseInt($("#add2").val());
-alert(add(number1, number2));
 });
