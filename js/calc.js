83a966d6a62e35b4a1c2f58006cb6b2d67ff7554
lulumoha <lulumoha123@gmail.com>
2017-11-15 11:18:34 +0300

    added code to calc.js

---
+++ js/calc.js
@@ -0,0 +1,15 @@
+// Business (or back-end) logic:
+var add = function(number1, number2){
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2){
+  return number1 - number2;
+};
+var multiply = function(number1, number2){
+  return number1 * number2;
+};
+
+var divide = function(number1, number2){
+  return number1 / number2;
+};
