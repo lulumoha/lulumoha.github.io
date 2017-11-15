6e1eac66c7cd92c6f5eaf3e20c3e8d2574bb7639
lulumoha <lulumoha123@gmail.com>
2017-11-15 12:12:20 +0300

    add modifications

--- js/calc.js
+++ js/calc.js
@@ -6,7 +6,7 @@ var add = function(number1, number2){
 var subtract = function(number1, number2){
   return number1 - number2;
 };
-var multiply = function(number1, number2){
+var subtract = function(number1, number2){
   return number1 * number2;
 };
 
@@ -35,6 +35,16 @@ $(document).ready(function() {
   var number1 = parseInt($("#subtract1").val());
   var number2 = parseInt($("#subtract2").val());
   var result = subtract(number1, number2);
+  $("#output").text(result);
+});
+});
+
+$(document).ready(function() {
+  $("form#multiply").submit(function(event) {
+ event.preventDefault();
+  var number1 = parseInt($("#multiply1").val());
+  var number2 = parseInt($("#multiply2").val());
+  var result = multiply(number1, number2);
   $("#output").text(result);
 });
 });
